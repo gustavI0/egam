0b08de10813631081dd8278dc0009da4bf1b13b0
jguyon <gustavI0@gustetto.net>
2024-04-16 10:42:13 +0200

    Screenshot list commented

--- web/modules/custom/egam_screenshot/src/ScreenshotListBuilder.php
+++ web/modules/custom/egam_screenshot/src/ScreenshotListBuilder.php
@@ -18,10 +18,9 @@ final class ScreenshotListBuilder extends EntityListBuilder {
   public function buildHeader(): array {
     $header['id'] = $this->t('ID');
     $header['label'] = $this->t('Label');
-    $header['artwork'] = $this->t('Artwork');
-    $header['game'] = $this->t('Game');
+//    $header['artwork'] = $this->t('Artwork');
+//    $header['game'] = $this->t('Game');
     $header['status'] = $this->t('Status');
-    $header['uid'] = $this->t('Author');
     $header['created'] = $this->t('Created');
     $header['changed'] = $this->t('Updated');
     return $header + parent::buildHeader();
@@ -34,14 +33,9 @@ final class ScreenshotListBuilder extends EntityListBuilder {
     /** @var \Drupal\egam_screenshot\ScreenshotInterface $entity */
     $row['id'] = $entity->id();
     $row['label'] = $entity->toLink();
-    $row['artwork'] = !$entity->get('field_artwork')->isEmpty() ? Artwork::load($entity->get('field_artwork')->target_id)->toLink() : NULL;
-    $row['game'] = !$entity->get('field_game')->isEmpty() ? Game::load($entity->get('field_game')->target_id)->toLink() : NULL;
+//    $row['artwork'] = !$entity->get('field_artwork')->isEmpty() ? Artwork::load($entity->get('field_artwork')->target_id)->toLink() : NULL;
+//    $row['game'] = !$entity->get('field_game')->isEmpty() ? Game::load($entity->get('field_game')->target_id)->toLink() : NULL;
     $row['status'] = $entity->get('status')->value ? $this->t('Enabled') : $this->t('Disabled');
-    $username_options = [
-      'label' => 'hidden',
-      'settings' => ['link' => $entity->get('uid')->entity->isAuthenticated()],
-    ];
-    $row['uid']['data'] = $entity->get('uid')->view($username_options);
     $row['created']['data'] = $entity->get('created')->view(['label' => 'hidden']);
     $row['changed']['data'] = $entity->get('changed')->view(['label' => 'hidden']);
     return $row + parent::buildRow($entity);
