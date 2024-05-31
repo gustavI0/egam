0fd7c572fd3b43a19ebf7e761e8257bcd3717de2
jguyon <gustavI0@gustetto.net>
2024-05-31 16:19:22 +0200

    Active links color + fixed header

--- web/themes/custom/egam/assets/dist/js/global.js
+++ web/themes/custom/egam/assets/dist/js/global.js
@@ -9,7 +9,16 @@
 
   Drupal.behaviors.egam = {
     attach: function (context, settings) {
+      // When the user scrolls down 50px from the top of the document, resize the header's font size
+      window.onscroll = function() {scrollFunction()};
 
+      function scrollFunction() {
+        if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+          document.getElementById("header").classList.add("small");
+        } else {
+          document.getElementById("header").classList.remove("small");
+        }
+      }
     }
   };
 
